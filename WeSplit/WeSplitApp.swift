a947f4bfad5c240c9095f11f882e82ffedc0b5b7
Matthew Dolan <matthew.dolan@ballysinteractive.com>
2022-09-20 12:40:36 -0700

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Matthew Dolan on 2022-09-20.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
