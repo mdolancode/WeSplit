4dc45f5aaf85620217432d43eaacd7910eefdcf2
Matthew Dolan <matthew.dolan@ballysinteractive.com>
2022-09-20 13:38:14 -0700

    Reset project

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,18 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    let students = ["Harry", "Hermione", "Ron"]
-    @State private var selectedStudent = "Harry"
     var body: some View {
-        NavigationView {
-            Form {
-                Picker("Select your student", selection: $selectedStudent) {
-                    ForEach(students, id: \.self) {
-                        Text($0)
-                    }
-                }
-            }
-        }
+        Text("Hello, world!")
+            .padding()
     }
 }
 
